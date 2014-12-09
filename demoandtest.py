e11c3b5271f603d238190339d0c7b025936cee37
David Jahn <David.Jahn@staff.uni-marburg.de>
2014-12-09 07:51:18 +0100

    Added some comments to demoandtest.py

--- demoandtest.py
+++ demoandtest.py
@@ -3,37 +3,51 @@ import TeraData
 import glob
 import pylab as py
 
-path2='/home/jahndav/Dropbox/THz-Analysis/'    
-#    samfiles=glob.glob(path2+'MarburgData/*_Lact1*')
+
+#provide a path to the reference and sample files
+#a list of strings or a glob expression can be used
 samfiles=glob.glob('/home/jahndav/Dropbox/THz-Analysis/rehi/Sample_?.txt')
 reffiles=glob.glob('/home/jahndav/Dropbox/THz-Analysis/rehi/Ref*.txt')
 
+#use the appropriate importer for the Data,
+#initialize the TimeDomain Data Object with a list of filenames
 sam_td=TeraData.ImportMarburgData(samfiles)
 ref_td=TeraData.ImportMarburgData(reffiles)
 
-#the origginal fdData:
+#the Frequency domain data is calculated by passing a TimeDomain data object to the FdData class
 sam_fd=TeraData.FdData(sam_td)
 ref_fd=TeraData.FdData(ref_td)
+
+#the Transferfunction is a object of type HMeas, which is nothing than a special frequency domain 
+#data type, such that HMeas inherits from FdData
 H=Terapy.HMeas(ref_fd,sam_fd)
 
-#the windowed data
+##################################################################################################
+# until here was a demonstration how the objects can be initialized
+##################################################################################################
+
+#the time domain data can be manipulatd through the member functions of THzTdData, so for instance
+#windowing:
 sam_td.setTDData(sam_td.getWindowedData(1e-12))
 ref_td.setTDData(ref_td.getWindowedData(1e-12))
 
+#create new frequency domain data, with the windowed timedomain data as input
 sam_fd_windowed=TeraData.FdData(sam_td)
 ref_fd_windowed=TeraData.FdData(ref_td)
 
 H_windowed=Terapy.HMeas(ref_fd_windowed,sam_fd_windowed)
 
-#the zeropadded and windowed data
+#Zeropadding in time domain
 sam_td.zeroPaddData(sam_td.getLength()*2)
 ref_td.zeroPaddData(ref_td.getLength()*2)
 
+#create new FdData array
 sam_fd_zpandwind=TeraData.FdData(sam_td)
 ref_fd_zpandwind=TeraData.FdData(ref_td)
 
 H_zpandwind=Terapy.HMeas(ref_fd_zpandwind,sam_fd_zpandwind)
 
+#do some plots
 #py.figure(1)
 H.doPlot()
 H_windowed.doPlot()
